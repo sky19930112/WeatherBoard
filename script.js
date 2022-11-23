dd6809c6abff81cef7194d0ae717f652d39039eb
sky19930112 <107167603+sky19930112@users.noreply.github.com>
2022-11-22 18:06:44 -0600

    link to weather api

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 var searchInput = document.querySelector('#searchInput');
 var searchBtn = document.querySelector('#searchBtn');
-
+var weatherApi = "https://api.openweathermap.org";
+var ApiKey = "09878fa39ce4cbfcc644551af0fcc59c"
 
 function InputSearch() {
   var search = searchInput.value;
   console.log(search)
+  axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${search}&appid=${ApiKey}`)
+  .then(res => {
+    console.log(res.data);
+  })
  
 }
 
