e8460ee8c04036c170ae55b41a0aaf538c77ae0b
sky19930112 <107167603+sky19930112@users.noreply.github.com>
2022-11-22 18:44:57 -0600

    work on cards and api link data

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 var searchInput = document.querySelector('#searchInput');
 var searchBtn = document.querySelector('#searchBtn');
+var today = document.querySelector('#today');
+var future = document.querySelector('#future');
 var weatherApi = "https://api.openweathermap.org";
 var ApiKey = "09878fa39ce4cbfcc644551af0fcc59c"
 
 function InputSearch() {
   var search = searchInput.value;
   console.log(search)
-  axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${search}&appid=${ApiKey}`)
+  axios.get(`${weatherApi}/data/2.5/forecast?q=${search}&appid=${ApiKey}`)
   .then(res => {
     console.log(res.data);
+    var lat = res.data.city.coord.lat;
+    var lon = res.data.city.coord.lon;
+    axios.get(`${weatherApi}/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${ApiKey}`)
+    .then(res => {
+      console.log(res.data);
+      today.innerHTML = `${res.data.city.name}`
+    })
   })
  
 }
